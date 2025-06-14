e9e941dda3575ff8496eea57c7e336ea17dfe621
Allber Fellype Soares Ferreira <alferreira@id.uff.br>
2025-06-14 20:33:09 -0300

    Update router.tsx

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
         element: <HomePage />
       },
       {
-        path: "profile",
+        path: "/profile",
         element: <ProfilePage />
       }
     ]
